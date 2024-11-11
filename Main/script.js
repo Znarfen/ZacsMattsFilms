bcb4e3f0833c62fbb2d5d4553eb91f73a08eba78
Znarfen <znarfstrom@gmail.com>
2024-11-11 18:38:58 +0100

    added movie thumnals

---
+++ Main/script.js
@@ -0,0 +1,33 @@
+
+for (let i = 0; i < 5; i++) {
+    loadThumbnails("dummy1")
+    loadThumbnails("dummy2")
+    loadThumbnails("dummy3")
+}
+
+// Load movies
+function loadThumbnails(film) {
+    console.log("films: " + film);
+
+    // Create actual image for thumbnail
+    var pick = document.createElement("img")
+    pick.setAttribute("src", "../Films/" + film + "/img.jpg");
+    
+    // div for enier thumbnail
+    var thumbnail = document.createElement("div");
+    thumbnail.id = film;
+    thumbnail.href = film;
+    thumbnail.className = "thumnail";
+
+    // Add elements to div and div to p and p to body
+    thumbnail.appendChild(pick);
+    document.getElementById("movies").appendChild(thumbnail);
+
+    thumbnail.onclick = function() {startFilm(film)};
+}
+
+function startFilm(film) {
+    console.log("Running film: " + film)
+    window.location = "#" + film;
+}
+
